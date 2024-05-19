c427fde27c8ead1d94b6aff6dc2a2ee8c92eb33e
Anna Kirillova <a.kirillova1812@gmail.com>
2024-05-19 11:42:22 +0400

    Move JsonUtil to test

--- src/main/java/ru/kirillova/bankservice/config/AppConfig.java
+++ src/main/java/ru/kirillova/bankservice/config/AppConfig.java
@@ -1,34 +1,9 @@
 package ru.kirillova.bankservice.config;
 
-import com.fasterxml.jackson.annotation.JsonAnyGetter;
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.hibernate5.jakarta.Hibernate5JakartaModule;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.ProblemDetail;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import ru.kirillova.bankservice.util.JsonUtil;
-
-import java.util.Map;
-
-import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.ANY;
-import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.NONE;
 
 @Configuration
 @EnableScheduling
 public class AppConfig {
-
-    @Autowired
-    void configureAndStoreObjectMapper(ObjectMapper objectMapper) {
-        objectMapper.registerModule(new Hibernate5JakartaModule());
-        objectMapper.addMixIn(ProblemDetail.class, MixIn.class);
-        JsonUtil.setMapper(objectMapper);
-    }
-
-    @JsonAutoDetect(fieldVisibility = NONE, getterVisibility = ANY)
-    interface MixIn {
-        @JsonAnyGetter
-        Map<String, Object> getProperties();
-    }
 }
